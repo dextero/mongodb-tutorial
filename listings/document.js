b23b53ac5406ae298c3d0bbc7363cf1a1ccff2fb
dextero <hukutizuviki+git@gmail.com>
2014-04-23 20:05:19 +0200

    Dodane obrazki i listingi w zewnetrznych plikach

---
+++ listings/document.js
@@ -0,0 +1,11 @@
+// posts/1
+{
+  "blog": "blogs/1",
+  "title": "RavenDB",
+  "content": "... content ...",
+  "categories": ["Raven", "NoSQL"],
+  "tags" : ["RavenDB", "Announcements"],
+  "comments": [
+    { "content": "Great News" }
+  ]
+}
